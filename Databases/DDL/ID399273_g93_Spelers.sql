7d098b860e9a87fe3588d8c96a917e146a4c0b9f
Arthur Van Ginderachter <arthur@vanginderachter.be>
2025-04-25 14:33:50 +0200

    init old splendor ap 2022-2023

---
+++ Databases/DDL/ID399273_g93_Spelers.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: ID399273_g93.db.webhosting.be    Database: ID399273_g93
+-- ------------------------------------------------------
+-- Server version	5.7.40-43-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Spelers`
+--
+
+DROP TABLE IF EXISTS `Spelers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Spelers` (
+  `idspeler` int(11) NOT NULL AUTO_INCREMENT,
+  `gebruikersnaam` varchar(20) NOT NULL,
+  `geboortejaar` year(4) NOT NULL,
+  PRIMARY KEY (`idspeler`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Spelers`
+--
+
+LOCK TABLES `Spelers` WRITE;
+/*!40000 ALTER TABLE `Spelers` DISABLE KEYS */;
+INSERT INTO `Spelers` VALUES (1,'test1',1999),(2,'test2',1999),(3,'test3',1999),(4,'test4',1998),(5,'test5',1999);
+/*!40000 ALTER TABLE `Spelers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-14 15:20:54
---
+++ Databases/Mysql/UC1.sql
@@ -0,0 +1,6 @@
+//Spelers
+CREATE TABLE `ID399273_g93`.`Spelers` (
+  `idspeler` INT NOT NULL,
+  `gebruikersnaam` VARCHAR(20) NOT NULL,
+  `geboortejaar` YEAR NOT NULL,
+  PRIMARY KEY (`idspeler`));
